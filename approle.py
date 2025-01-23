17188ef359fc621ab0775516458bf3e451c8c330
Denish Borad <denishgithub4253@gmail.com>
2025-01-23 23:24:11 +0530

    role base set login and set that data show permission
    role base set login and set that data show permission

--- approle.py
+++ approle.py
@@ -1,12 +1,12 @@
 # import Flask from flask
-from flask import Flask
+from flask import Flask, request
 # and store the instance in 'db'
 from flask_sqlalchemy import SQLAlchemy
 # import UserMixin, RoleMixin
-from flask_security import Security, SQLAlchemySessionUserDatastore, UserMixin, RoleMixin
+from flask_security import Security, SQLAlchemySessionUserDatastore, UserMixin, RoleMixin, roles_accepted
 from flask_migrate import Migrate
 # import required libraries from flask_login and flask_security
-# from flask_login import LoginManager, login_manager, login_user
+from flask_login import LoginManager, login_manager, login_user
 
 # import the required libraries
 from flask import render_template, redirect, url_for
@@ -45,27 +45,157 @@ roles_users = db.Table('roles_users',
 # create table in database for storing users
 class User(db.Model, UserMixin):
     __tablename__ = 'user'
-    id          = db.Column(db.Integer, autoincrement=True, primary_key=True)
-    email       = db.Column(db.String, unique=True)
-    password    = db.Column(db.String(255), nullable=False, server_default='')
-    active      = db.Column(db.Boolean())
+    id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    email = db.Column(db.String, unique=True)
+    password = db.Column(db.String(255), nullable=False, server_default='')
+    active = db.Column(db.Boolean())
+    fs_uniquifier = db.Column(db.String(255), unique=True, nullable=False)  # Added field
     # backreferences the user_id from roles_users table
-    roles       = db.relationship('Role', secondary=roles_users, backref='roled')
+    roles = db.relationship('Role', secondary=roles_users, backref='roled')
+
+    # Ensure fs_uniquifier is always set
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        if not self.fs_uniquifier:
+            import uuid
+            self.fs_uniquifier = str(uuid.uuid4())
+
 
 # create table in database for storing roles
 class Role(db.Model, RoleMixin):
     __tablename__ = 'role'
     id      = db.Column(db.Integer(), primary_key=True)
     name    = db.Column(db.String(80), unique=True)
-    
+
 # creates all database tables
 def create_tables():
     db.create_all()
 
+# @app.before_request
+# def initialize_roles():
+#     from create_roles import create_roles
+#     create_roles()
+
+
+
+user_datastore = SQLAlchemySessionUserDatastore(db.session, User, Role)
+security = Security(app, user_datastore)
+
+
 @app.route('/')
 def index():
-    return render_template("approle.html")
+    return render_template("role/approle.html")
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    msg=""
+    # if the form is submitted
+    if request.method == 'POST':
+    # check if user already exists
+        user = User.query.filter_by(email=request.form['email']).first()
+        msg=""
+        # if user already exists render the msg
+        if user:
+            msg="User already exist"
+            # render signup.html if user exists
+            return render_template('signup.html', msg=msg)
+        
+        # if user doesn't exist
+        
+        # store the user to database
+        user = User(email=request.form['email'], active=1, password=request.form['password'])
+        # store the role
+        role = Role.query.filter_by(id=request.form['options']).first()
+        user.roles.append(role)
+        
+        # commit the changes to database
+        db.session.add(user)
+        db.session.commit()
+        
+        # login the user to the app
+        # this user is current user
+        login_user(user)
+        # redirect to index page
+        return redirect(url_for('index'))
+        
+    # case other than submitting form, like loading the page itself
+    else:
+        return render_template("role/rolesignup.html", msg=msg)
+
+# signin page
+@app.route('/signin', methods=['GET', 'POST'])
+def signin():
+    msg=""
+    if request.method == 'POST':
+        # search user in database
+        user = User.query.filter_by(email=request.form['email']).first()
+        # if exist check password
+        if user:
+            if  user.password == request.form['password']:
+                # if password matches, login the user
+                login_user(user)
+                return redirect(url_for('index'))
+            # if password doesn't match
+            else:
+                msg="Wrong password"
+        
+        # if user does not exist
+        else:
+            msg="User doesn't exist"
+        return render_template('role/rolesignin.html', msg=msg)
+        
+    else:
+        return render_template("role/rolesignin.html", msg=msg)
+
+
+@app.route('/teachers')
+# only Admin can access the page
+@roles_accepted('Admin')
+def teachers():
+    teachers = []
+    # query for role Teacher that is role_id=2
+    role_teachers = db.session.query(roles_users).filter_by(role_id=2)
+    # query for the users' details using user_id
+    for teacher in role_teachers:
+        user = User.query.filter_by(id=teacher.user_id).first()
+        teachers.append(user)
+    # return the teachers list
+    return render_template("role/roleteachers.html", teachers=teachers)
+
+# for staff page
+@app.route('/staff')
+# only Admin and Teacher can access the page
+@roles_accepted('Admin', 'Teacher')
+def staff():
+    staff = []
+    role_staff = db.session.query(roles_users).filter_by(role_id=3)
+    for staf in role_staff:
+        user = User.query.filter_by(id=staf.user_id).first()
+        staff.append(user)
+    return render_template("role/rolestaff.html", staff=staff)
+    
+# for student page
+@app.route('/students')
+# only Admin, Teacher and Staff can access the page
+@roles_accepted('Admin', 'Teacher', 'Staff')
+def students():
+    students = []
+    role_students = db.session.query(roles_users).filter_by(role_id=4)
+    for student in role_students:
+        user = User.query.filter_by(id=student.user_id).first()
+        students.append(user)
+    return render_template("role/rolestudents.html", students=students)
+    
+# for details page
+@app.route('/mydetails')
+# Admin, Teacher, Staff and Student can access the page
+@roles_accepted('Admin', 'Teacher', 'Staff', 'Student')
+def mydetails():
+    return render_template("role/rolemydetails.html")
 
 # Run code in debug mode
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+
--- create_roles.py
+++ create_roles.py
@@ -1,19 +1,46 @@
-# create_roles.py
-from approle import Role, User, db
+from approle import Role, db
 
 def create_roles():
-    admin = Role(id=1, name='Admin')
-    teacher = Role(id=2, name='Teacher')
-    staff = Role(id=3, name='Staff')
-    student = Role(id=4, name='Student')
+    # Define roles
+    roles = [
+        {'id': 1, 'name': 'Admin'},
+        {'id': 2, 'name': 'Teacher'},
+        {'id': 3, 'name': 'Staff'},
+        {'id': 4, 'name': 'Student'}
+    ]
 
-    db.session.add(admin)
-    db.session.add(teacher)
-    db.session.add(staff)
-    db.session.add(student)
+    # Add roles if they do not already exist
+    for role in roles:
+        existing_role = Role.query.filter_by(id=role['id']).first()
+        if not existing_role:
+            new_role = Role(id=role['id'], name=role['name'])
+            db.session.add(new_role)
 
     db.session.commit()
     print("Roles created successfully!")
 
-# Function calling will create 4 roles as planned!
-create_roles()
+if __name__ == "__main__":
+    create_roles()
+
+
+
+
+# # create_roles.py
+# from approle import Role, User, db
+
+# def create_roles():
+#     admin = Role(id=1, name='Admin')
+#     teacher = Role(id=2, name='Teacher')
+#     staff = Role(id=3, name='Staff')
+#     student = Role(id=4, name='Student')
+
+#     db.session.add(admin)
+#     db.session.add(teacher)
+#     db.session.add(staff)
+#     db.session.add(student)
+
+#     db.session.commit()
+#     print("Roles created successfully!")
+
+# # Function calling will create 4 roles as planned!
+# create_roles()
